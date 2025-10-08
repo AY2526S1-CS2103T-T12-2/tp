2ec8a60ae199bf9e78397f086e84380e2637fd8e
wjingsin <wongjingsin123@gmail.com>
2025-10-08 22:53:08 +0800

    Improved List Students classes

--- src/main/java/seedu/tabs/logic/commands/ListStudentsCommand.java
+++ src/main/java/seedu/tabs/logic/commands/ListStudentsCommand.java
@@ -1,8 +1,15 @@
 package seedu.tabs.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.tabs.logic.parser.CliSyntax.PREFIX_TUTORIAL_ID;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import seedu.tabs.logic.commands.exceptions.CommandException;
 import seedu.tabs.model.Model;
+import seedu.tabs.model.student.Student;
+import seedu.tabs.model.tutorial.Tutorial;
 import seedu.tabs.model.tutorial.TutorialId;
 
 /**
@@ -12,18 +19,44 @@ public class ListStudentsCommand extends Command {
 
     public static final String COMMAND_WORD = "list_students";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": lists all the students in a tutorial"
+            + "Parameters: "
+            + PREFIX_TUTORIAL_ID + "TUTORIAL ID";
+
     // dummy values
-    private String listOfStudents = "1. A0303333Z \n2. A0303334Z \n3. A0303335Z \n4. A0303336Z";
+    private String listOfStudents = "";
 
     private final TutorialId tutorialId;
+
 
     public ListStudentsCommand(TutorialId tutorialId) {
         this.tutorialId = tutorialId;
     }
 
     @Override
-    public CommandResult execute(Model model) {
+    public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
+
+        // Get tutorial based on tutorialId given
+        Tutorial tutorial = model.getTAbs().getTutorialList().stream()
+                .filter(tut -> tut.getTutorialId().equals(tutorialId)).findFirst()
+                .orElseThrow(() -> new CommandException(
+                        String.format("A tutorial with the TUTORIAL_ID %s does not exist", tutorialId.fullName)
+                ));
+
+        // Convert the set of students to a list to preserve order
+        List<Student> studentList = new ArrayList<>(tutorial.getStudents());
+
+        if (studentList.size() == 0) {
+            throw new CommandException(
+                    String.format("A tutorial with the TUTORIAL_ID %s does not exist", tutorialId.fullName)
+            );
+        }
+
+        for (int i = 0; i < studentList.size(); i++) {
+            listOfStudents = listOfStudents + (i + 1) + ". " + studentList.get(i).studentName + "\n";
+        }
+
         return new CommandResult(listOfStudents);
     }
 }
--- src/main/java/seedu/tabs/logic/parser/ListStudentsCommandParser.java
+++ src/main/java/seedu/tabs/logic/parser/ListStudentsCommandParser.java
@@ -1,11 +1,15 @@
 package seedu.tabs.logic.parser;
 
 import static seedu.tabs.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.tabs.logic.parser.CliSyntax.PREFIX_TUTORIAL_ID;
 
-import seedu.tabs.logic.commands.DeleteCommand;
+import java.util.stream.Stream;
+
 import seedu.tabs.logic.commands.ListStudentsCommand;
 import seedu.tabs.logic.parser.exceptions.ParseException;
 import seedu.tabs.model.tutorial.TutorialId;
+
+
 
 /**
  * Parses input arguments and creates a new DeleteCommand object
@@ -18,13 +22,24 @@ public class ListStudentsCommandParser implements Parser<ListStudentsCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public ListStudentsCommand parse(String args) throws ParseException {
-        try {
-            TutorialId tutorialId = ParserUtil.parseName(args);
-            return new ListStudentsCommand(tutorialId);
-        } catch (ParseException pe) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_TUTORIAL_ID);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_TUTORIAL_ID) || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListStudentsCommand.MESSAGE_USAGE));
         }
+
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_TUTORIAL_ID);
+
+        TutorialId tutorialId = ParserUtil.parseName(argMultimap.getValue(PREFIX_TUTORIAL_ID).get());
+
+        return new ListStudentsCommand(tutorialId);
+    }
+    /**
+     * Returns true if none of the prefixes contain empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 
 }
