9f1e8e807fbc036ddfa4b496a0b1eb3ee91645d9
203ZK <houjinwork@gmail.com>
2025-10-08 23:51:47 +0800

    Add TutorialIdMatchesKeywordPredicate.java

---
+++ src/main/java/seedu/tabs/model/tutorial/TutorialIdMatchesKeywordPredicate.java
@@ -0,0 +1,37 @@
+package seedu.tabs.model.tutorial;
+
+import java.util.function.Predicate;
+
+import seedu.tabs.commons.util.ToStringBuilder;
+
+public class TutorialIdMatchesKeywordPredicate implements Predicate<Tutorial> {
+    private final String keyword;
+
+    public TutorialIdMatchesKeywordPredicate(String keyword) { this.keyword = keyword; }
+
+    @Override
+    public boolean test(Tutorial aTutorial) {
+        return keyword.equals(aTutorial.getTutorialId().fullName);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof TutorialIdMatchesKeywordPredicate)) {
+            return false;
+        }
+
+        TutorialIdMatchesKeywordPredicate otherTutorialIdMatchesKeywordPredicate =
+                (TutorialIdMatchesKeywordPredicate) other;
+        return keyword.equals(otherTutorialIdMatchesKeywordPredicate.keyword);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).add("keyword", keyword).toString();
+    }
+}
