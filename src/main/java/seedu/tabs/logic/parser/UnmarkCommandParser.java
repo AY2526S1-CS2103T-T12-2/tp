77e7c27cd29e44a500120f743f42c9376626735a
dlywlotus <dylanlowywgit@gmail.com>
2025-10-21 20:11:07 +0800

    feat/unmark-command

---
+++ src/main/java/seedu/tabs/logic/parser/UnmarkCommandParser.java
@@ -0,0 +1,60 @@
+package seedu.tabs.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.tabs.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.tabs.logic.parser.CliSyntax.PREFIX_STUDENT;
+import static seedu.tabs.logic.parser.CliSyntax.PREFIX_TUTORIAL_ID;
+
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import seedu.tabs.logic.commands.UnmarkCommand;
+import seedu.tabs.logic.parser.exceptions.ParseException;
+import seedu.tabs.model.student.Student;
+import seedu.tabs.model.tutorial.TutorialIdMatchesKeywordPredicate;
+
+/**
+ * Parses input arguments and creates a new UnmarkCommand object
+ */
+public class UnmarkCommandParser {
+    /**
+     * Parses the given {@code String} of arguments in the context of the UnmarkCommand
+     * and returns an UnmarkCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public UnmarkCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args,
+                PREFIX_STUDENT, PREFIX_TUTORIAL_ID);
+
+        boolean hasMissingPrefixes = !arePrefixesPresent(argMultimap, PREFIX_STUDENT, PREFIX_TUTORIAL_ID);
+        boolean hasNonEmptyPreamble = !argMultimap.getPreamble().isEmpty();
+        if (hasMissingPrefixes || hasNonEmptyPreamble) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    UnmarkCommand.MESSAGE_USAGE));
+        }
+
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_TUTORIAL_ID);
+        Set<Student> studentSet = ParserUtil.parseStudents(argMultimap.getAllValues(PREFIX_STUDENT));
+
+        String tutorialId;
+        try {
+            tutorialId = argMultimap.getValue(PREFIX_TUTORIAL_ID).orElseThrow();
+        } catch (NoSuchElementException e) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    UnmarkCommand.MESSAGE_USAGE));
+        }
+
+        return new UnmarkCommand(studentSet, new TutorialIdMatchesKeywordPredicate(tutorialId));
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+}
