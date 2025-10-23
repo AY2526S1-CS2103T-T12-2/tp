c10eabe1c3704ea55c279682b79c616f3fde87cf
wjingsin <wongjingsin123@gmail.com>
2025-10-23 11:29:39 +0800

    Fix style

--- src/main/java/seedu/tabs/model/tutorial/TutorialIdContainsKeywordsPredicate.java
+++ src/main/java/seedu/tabs/model/tutorial/TutorialIdContainsKeywordsPredicate.java
@@ -3,7 +3,6 @@ package seedu.tabs.model.tutorial;
 import java.util.List;
 import java.util.function.Predicate;
 
-import seedu.tabs.commons.util.StringUtil;
 import seedu.tabs.commons.util.ToStringBuilder;
 
 /**
